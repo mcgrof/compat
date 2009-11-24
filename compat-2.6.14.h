917dd4a0f693307d427d17f9556f12320a60f022
Arnaud Lacombe <lacombar@gmail.com>
2009-11-23 21:23:59 -0500

    Add typedef for `gfp_t'
    Added on commit dd0fc66f by Al Viro in include/linux/types.h.
    
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- compat-2.6.14.h
+++ compat-2.6.14.h
@@ -7,6 +7,8 @@
 /* Compat work for 2.6.14 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,14))
 
+typedef unsigned int gfp_t;
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,14)) */
 
 #endif /* LINUX_26_14_COMPAT_H */
