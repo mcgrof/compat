802708a593aa035e3c4d5179842a36c5f2539e08
Arnaud Lacombe <lacombar@gmail.com>
2009-11-23 21:24:15 -0500

    Add roundup() compatibility macro
    Added on commit b4cac1a0 by David Howells in include/linux/kernel.h.
    
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- compat-2.6.18.h
+++ compat-2.6.18.h
@@ -7,6 +7,8 @@
 /* Compat work for 2.6.18 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18))
 
+#define roundup(x, y)	((((x) + ((y) - 1)) / (y)) * (y))
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18)) */
 
 #endif /* LINUX_26_18_COMPAT_H */
