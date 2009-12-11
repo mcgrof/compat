416dea1cc82b74ffb0bf2782c24fc0557170b8cb
Luis R. Rodriguez <lrodriguez@atheros.com>
2009-12-10 23:30:08 -0800

    Add some new 2.6.33 backport code for networking
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- include/linux/compat-2.6.33.h
+++ include/linux/compat-2.6.33.h
@@ -10,6 +10,12 @@
 #define IFF_DONT_BRIDGE 0x800		/* disallow bridging this ether dev */
 /* source: include/linux/if.h */
 
+#define kparam_block_sysfs_write(a)
+#define kparam_unblock_sysfs_write(a)
+
+/* this will never happen on older kernels */
+#define NETDEV_POST_INIT 0xffff
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,33)) */
 
 #endif /* LINUX_26_33_COMPAT_H */
