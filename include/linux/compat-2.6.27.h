819330bf6c14af584b494e590df68cbca388a921
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-01-11 12:18:59 -0800

    Skip MMC/SDIO backport for kernels <= 2.6.23
    This allows compilation of compat for 2.6.23 for builds
    that do not require MMC/SDIO. Anyone interested in MMC/SDIO
    backport support should look to see if its possible to
    backport/add MMC/SDIO support to older kernels. That's
    likeley a project on its own though, not sure.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- include/linux/compat-2.6.27.h
+++ include/linux/compat-2.6.27.h
@@ -9,8 +9,10 @@
 #include <linux/list.h>
 #include <linux/pci.h>
 #include <linux/dma-mapping.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24))
 #include <linux/mmc/sdio.h>
 #include <linux/mmc/sdio_func.h>
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24) */
 #include <linux/netdevice.h>
 #include <linux/workqueue.h>
 #include <net/iw_handler.h>
@@ -159,8 +161,10 @@ static inline void list_splice_tail_init(struct list_head *list,
 	}
 }
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24))
 extern unsigned int mmc_align_data_size(struct mmc_card *, unsigned int);
 extern unsigned int sdio_align_size(struct sdio_func *func, unsigned int sz);
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24) */
 
 #define iwe_stream_add_value(info, event, value, ends, iwe, event_len) iwe_stream_add_value(event, value, ends, iwe, event_len)
 #define iwe_stream_add_point(info, stream, ends, iwe, extra) iwe_stream_add_point(stream, ends, iwe, extra)
