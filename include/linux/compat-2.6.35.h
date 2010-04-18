26c30be706748b6beea1a062354fcc86c71b9611
Hauke Mehrtens <hauke@hauke-m.de>
2010-04-18 15:37:56 +0200

    compat: Add definitions needed for libertas sdio driver.
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/linux/compat-2.6.35.h
+++ include/linux/compat-2.6.35.h
@@ -8,6 +8,9 @@
 #define IW_HANDLER(id, func)			\
 	[IW_IOCTL_IDX(id)] = func
 
+#define  SDIO_BUS_ECSI		0x20	/* Enable continuous SPI interrupt */
+#define  SDIO_BUS_SCSI		0x40	/* Support continuous SPI interrupt */
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,35)) */
 
 #endif /* LINUX_26_35_COMPAT_H */
