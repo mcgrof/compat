7349fb77e7b24102f4539a8022e9fd75147b5012
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-05-04 16:43:05 -0700

    compat: add new way to backport the usage of net_device_ops
    We tend to #ifdef code that uses the new net_device_ops
    on kernels older than 2.6.29. This tends to create a burden
    on the upkeeping of the hunks. We can minimize our work by
    instead definining a new netdev_attach_ops() which will do
    our backport work for the older kernels and simply attach
    the ops for newer kernels.
    
    This should simplify backporting of network drivers
    considerably.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- compat/compat-2.6.29.c
+++ compat/compat-2.6.29.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007	Luis R. Rodriguez <mcgrof@winlab.rutgers.edu>
+ * Copyright 2007-2010	Luis R. Rodriguez <mcgrof@winlab.rutgers.edu>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,6 +14,26 @@
 
 #include <linux/usb.h>
 #include <linux/etherdevice.h>
+
+/*
+ * Expand this as drivers require more ops, for now this
+ * only sets the ones we need.
+ */
+void netdev_attach_ops(struct net_device *dev,
+		       const struct net_device_ops *ops)
+{
+#define SET_NETDEVOP(_op) (_op ? _op : NULL)
+	dev->open = SET_NETDEVOP(ops->ndo_open);
+	dev->stop = SET_NETDEVOP(ops->ndo_stop);
+	dev->hard_start_xmit = SET_NETDEVOP(ops->ndo_start_xmit);
+	dev->set_multicast_list = SET_NETDEVOP(ops->ndo_set_multicast_list);
+	dev->change_mtu = SET_NETDEVOP(ops->ndo_change_mtu);
+	dev->set_mac_address = SET_NETDEVOP(ops->ndo_set_mac_address);
+	dev->tx_timeout = SET_NETDEVOP(ops->ndo_tx_timeout);
+	dev->get_stats = SET_NETDEVOP(ops->ndo_get_stats);
+#undef SET_NETDEVOP
+}
+EXPORT_SYMBOL(netdev_attach_ops);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,23))
 #if defined(CONFIG_USB) || defined(CONFIG_USB_MODULE)
