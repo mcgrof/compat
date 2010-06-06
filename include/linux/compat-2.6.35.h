19f8dbdfac80f63be6968e7283a25ed46f804c87
Hauke Mehrtens <hauke@hauke-m.de>
2010-06-06 11:12:24 +0200

    compat: update bitops.h and wireless.h
    copy files from linux-next next-20100604
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/linux/compat-2.6.35.h
+++ include/linux/compat-2.6.35.h
@@ -13,9 +13,6 @@
 #define SHRT_MAX       ((s16)(USHRT_MAX>>1))
 #define SHRT_MIN       ((s16)(-SHRT_MAX - 1))
 
-#define IW_HANDLER(id, func)			\
-	[IW_IOCTL_IDX(id)] = func
-
 #define  SDIO_BUS_ECSI		0x20	/* Enable continuous SPI interrupt */
 #define  SDIO_BUS_SCSI		0x40	/* Support continuous SPI interrupt */
 
