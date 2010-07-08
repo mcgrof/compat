7b8245ada847664fd3bda3c05949d60085ec94e0
Hauke Mehrtens <hauke@hauke-m.de>
2010-07-08 23:11:58 +0200

    compat: fix tracpoint building
    trace/define_trace.h can be included more often and it have to.
    
    This fixes some errors like this:
    WARNING: "__tracepoint_iwlwifi_dev_rx" [/home/hauke/compat-wireless/compat-wireless-git/drivers/net/wireless/iwlwifi/iwlcore.ko] undefined!
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/trace/define_trace.h
+++ include/trace/define_trace.h
@@ -1,10 +1,5 @@
-#ifndef _COMPAT_TRACE_DEFINE_TRACE_H
-#define _COMPAT_TRACE_DEFINE_TRACE_H 1
-
 #include <linux/version.h>
 
 #if (LINUX_VERSION_CODE > KERNEL_VERSION(2,6,30))
 #include_next <trace/define_trace.h>
 #endif /* (LINUX_VERSION_CODE > KERNEL_VERSION(2,6,30)) */
-
-#endif	/* _COMPAT_TRACE_DEFINE_TRACE_H */
