a61410a8587b4a1458607f91ebc0b1d93a9c6447
Hauke Mehrtens <hauke@hauke-m.de>
2010-09-19 10:42:10 -0700

    compat: move pmcia backport code into compat-2.6.36.h
    We have all out backport headers in include/linux/compat-2.6.XX.h also the pcmcia
    part there.
    
    CC: Kshitij Kulshreshtha <kkhere.geo@gmail.com>
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/linux/compat-2.6.36.h
+++ include/linux/compat-2.6.36.h
@@ -6,6 +6,8 @@
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36))
 
 #include <linux/usb.h>
+#include <pcmcia/cistpl.h>
+#include <pcmcia/ds.h>
 
 #define kparam_block_sysfs_write(a)
 #define kparam_unblock_sysfs_write(a)
@@ -26,6 +28,33 @@ extern void compat_usb_unlink_anchored_urbs(struct usb_anchor *anchor);
 extern struct urb *compat_usb_get_from_anchor(struct usb_anchor *anchor);
 extern void compat_usb_scuttle_anchored_urbs(struct usb_anchor *anchor);
 #endif
+
+/**
+ * pcmcia_read_config_byte() - read a byte from a card configuration register
+ *
+ * pcmcia_read_config_byte() reads a byte from a configuration register in
+ * attribute memory.
+ */
+static inline int pcmcia_read_config_byte(struct pcmcia_device *p_dev, off_t where, u8 *val)
+{
+        int ret;
+        conf_reg_t reg = { 0, CS_READ, where, 0 };
+        ret = pcmcia_access_configuration_register(p_dev, &reg);
+        *val = reg.Value;
+        return ret;
+}
+
+/**
+ * pcmcia_write_config_byte() - write a byte to a card configuration register
+ *
+ * pcmcia_write_config_byte() writes a byte to a configuration register in
+ * attribute memory.
+ */
+static inline int pcmcia_write_config_byte(struct pcmcia_device *p_dev, off_t where, u8 val)
+{
+	conf_reg_t reg = { 0, CS_WRITE, where, val };
+	return pcmcia_access_configuration_register(p_dev, &reg);
+}
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36)) */
 
