1594b0d57d4fb20d8518d138f56a121cab78f1c8
Hauke Mehrtens <hauke@hauke-m.de>
2010-09-19 10:42:11 -0700

    compat: pcmcia/cs.h is removed form new kernels
    On older kernel versions we have to include pcmcia/cs_types.h and
    pcmcia/cs.h before pcmcia/cistpl.h.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/pcmcia/cistpl.h
+++ include/pcmcia/cistpl.h
@@ -1,10 +1,10 @@
-#ifndef _COMPAT_PCMCIA_CS_H
-#define _COMPAT_PCMCIA_CS_H
 #include <linux/version.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36)
 #include <pcmcia/cs_types.h>
 #endif
-
-#include_next <pcmcia/cs.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,37)
+#include <pcmcia/cs.h>
 #endif
+
+#include_next <pcmcia/cistpl.h>
