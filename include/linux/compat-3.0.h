449009dde3f04a91f008615989b81ee70b558472
Hauke Mehrtens <hauke@hauke-m.de>
2011-06-10 00:04:44 +0200

    compat: add device name in register_netdevice(dev)
    dev_alloc_name() is not called explicitly in the driver code any more,
    but it is done in register_netdevice(). This causes devices getting
    wrong names like "wlan%d". With this patch they get names like wlan0
    again.
    
    Add to compat-3.0-stable
    
    CC: Ignacy Gawedzki <i@lri.fr>

---
+++ include/linux/compat-3.0.h
@@ -0,0 +1,31 @@
+#ifndef LINUX_3_0_COMPAT_H
+#define LINUX_3_0_COMPAT_H
+
+#include <linux/version.h>
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,0,0))
+
+/*
+ * since commit 1c5cae815d19ffe02bdfda1260949ef2b1806171
+ * "net: call dev_alloc_name from register_netdevice" dev_alloc_name is
+ * called automatically. This is not implemented in older kernel
+ * versions so it will result in device wrong names.
+ */
+static inline int register_netdevice_name(struct net_device *dev)
+{
+	int err;
+
+	if (strchr(dev->name, '%')) {
+		err = dev_alloc_name(dev, dev->name);
+		if (err < 0)
+			return err;
+	}
+
+	return register_netdevice(dev);
+}
+
+#define register_netdevice(dev) register_netdevice_name(dev)
+
+#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,0,0)) */
+
+#endif /* LINUX_3_0_COMPAT_H */
