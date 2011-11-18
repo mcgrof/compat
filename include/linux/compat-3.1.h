545ece91974440f6447bbd27a44775bdaeee8212
John W. Linville <linville@tuxdriver.com>
2011-11-18 15:52:44 -0500

    compat: implement dummy security_sk_clone
    This has been defined in include/linux/security.h for some time, but was
    only given an EXPORT_SYMBOL for 3.1.  Add a compat_* definition to avoid
    breaking the module load with an undefined symbol.
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- include/linux/compat-3.1.h
+++ include/linux/compat-3.1.h
@@ -19,6 +19,17 @@
 	.prod_id = { NULL, NULL, (v3), NULL }, \
 	.prod_id_hash = { 0, 0, (vh3), 0 }, }
 
+/*
+ * This has been defined in include/linux/security.h for some time, but was
+ * only given an EXPORT_SYMBOL for 3.1.  Add a compat_* definition to avoid
+ * breaking the compile.
+ */
+#define security_sk_clone(a, b) compat_security_sk_clone(a, b)
+
+static inline void security_sk_clone(const struct sock *sk, struct sock *newsk)
+{
+}
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)) */
 
 #endif /* LINUX_3_1_COMPAT_H */
