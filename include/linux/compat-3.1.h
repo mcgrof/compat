e77058754c01b758ae1607eb8b7ffbddd13160c9
Felix Fietkau <nbd@openwrt.org>
2011-12-02 11:28:26 +0100

    compat: backport atomic64 support
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- include/linux/compat-3.1.h
+++ include/linux/compat-3.1.h
@@ -30,6 +30,18 @@ static inline void security_sk_clone(const struct sock *sk, struct sock *newsk)
 {
 }
 
+/*
+ * In many versions, several architectures do not seem to include an
+ * atomic64_t implementation, and do not include the software emulation from
+ * asm-generic/atomic64_t.
+ * Detect and handle this here.
+ */
+#include <asm/atomic.h>
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,31)) && !defined(ATOMIC64_INIT) && !defined(CONFIG_X86) && !((LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33)) && defined(CONFIG_ARM) && !defined(CONFIG_GENERIC_ATOMIC64))
+#include <asm-generic/atomic64.h>
+#endif
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)) */
 
 #endif /* LINUX_3_1_COMPAT_H */
