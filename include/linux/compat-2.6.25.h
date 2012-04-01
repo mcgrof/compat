067f4e12299e1e00f95ed95b1ce9180f2e4cb58c
Luis R. Rodriguez <mcgrof@frijolero.org>
2012-03-31 21:46:02 -0700

    compat: backport in.h shortcuts added by Joe (2658)
    This should help compilation on 2.6.24.
    
    mcgrof@tux ~/compat (git::master)$ ckmake
    Trying kernel                  3.3.0-030300rc2-generic	[OK]
    Trying kernel                     3.2.2-030202-generic	[OK]
    Trying kernel                    3.1.10-030110-generic	[OK]
    Trying kernel                    3.0.18-030018-generic	[OK]
    Trying kernel                  2.6.39-02063904-generic	[OK]
    Trying kernel                        2.6.38-13-generic	[OK]
    Trying kernel                  2.6.38-02063808-generic	[OK]
    Trying kernel                  2.6.37-02063706-generic	[OK]
    Trying kernel                  2.6.36-02063604-generic	[OK]
    Trying kernel                  2.6.35-02063512-generic	[OK]
    Trying kernel                  2.6.34-02063410-generic	[OK]
    Trying kernel                  2.6.33-02063305-generic	[OK]
    Trying kernel                  2.6.32-02063255-generic	[OK]
    Trying kernel                        2.6.31-22-generic	[OK]
    Trying kernel                  2.6.31-02063113-generic	[OK]
    Trying kernel                  2.6.30-02063010-generic	[OK]
    Trying kernel                  2.6.29-02062906-generic	[OK]
    Trying kernel                  2.6.28-02062810-generic	[OK]
    Trying kernel                    2.6.27-020627-generic	[OK]
    Trying kernel                    2.6.26-020626-generic	[OK]
    Trying kernel                    2.6.25-020625-generic	[OK]
    Trying kernel                    2.6.24-020624-generic	[OK]
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@frijolero.org>

--- include/linux/compat-2.6.25.h
+++ include/linux/compat-2.6.25.h
@@ -16,6 +16,76 @@
 #include <asm-generic/bug.h>
 #include <linux/pm_qos_params.h>
 #include <linux/pci.h>
+#include <linux/in.h>
+#include <linux/errno.h>
+
+/*
+ * backports 2658fa803111dae1353602e7f586de8e537803e2
+ * We skip proto_ports_offset() as I'm lazy.
+ */
+
+static inline bool ipv4_is_loopback(__be32 addr)
+{
+	return (addr & htonl(0xff000000)) == htonl(0x7f000000);
+}
+
+static inline bool ipv4_is_multicast(__be32 addr)
+{
+	return (addr & htonl(0xf0000000)) == htonl(0xe0000000);
+}
+
+static inline bool ipv4_is_local_multicast(__be32 addr)
+{
+	return (addr & htonl(0xffffff00)) == htonl(0xe0000000);
+}
+
+static inline bool ipv4_is_lbcast(__be32 addr)
+{
+	/* limited broadcast */
+	return addr == htonl(INADDR_BROADCAST);
+}
+
+static inline bool ipv4_is_zeronet(__be32 addr)
+{
+	return (addr & htonl(0xff000000)) == htonl(0x00000000);
+}
+
+/* Special-Use IPv4 Addresses (RFC3330) */
+
+static inline bool ipv4_is_private_10(__be32 addr)
+{
+	return (addr & htonl(0xff000000)) == htonl(0x0a000000);
+}
+
+static inline bool ipv4_is_private_172(__be32 addr)
+{
+	return (addr & htonl(0xfff00000)) == htonl(0xac100000);
+}
+
+static inline bool ipv4_is_private_192(__be32 addr)
+{
+	return (addr & htonl(0xffff0000)) == htonl(0xc0a80000);
+}
+
+static inline bool ipv4_is_linklocal_169(__be32 addr)
+{
+	return (addr & htonl(0xffff0000)) == htonl(0xa9fe0000);
+}
+
+static inline bool ipv4_is_anycast_6to4(__be32 addr)
+{
+	return (addr & htonl(0xffffff00)) == htonl(0xc0586300);
+}
+
+static inline bool ipv4_is_test_192(__be32 addr)
+{
+	return (addr & htonl(0xffffff00)) == htonl(0xc0000200);
+}
+
+static inline bool ipv4_is_test_198(__be32 addr)
+{
+	return (addr & htonl(0xfffe0000)) == htonl(0xc6120000);
+}
 
 /*
  * phys_addr_t was added as a generic arch typedef on 2.6.28,
