621195cb683a97c5233fb036adb9e6c3af416e54
Luis R. Rodriguez <mcgrof@frijolero.org>
2012-05-16 21:03:59 -0700

    compat: backport net_ratelimited_function()
    This backports this patch:
    
    	From 3a3bfb61e64476ff1e4ac3122cb6dec9c79b795c Mon Sep 17 00:00:00 2001
    	From: Joe Perches <joe@perches.com>
    	Date: Sun, 13 May 2012 21:56:25 +0000
    	Subject: [PATCH] net: Add net_ratelimited_function and
    	 net_<level>_ratelimited macros
    
    	__ratelimit() can be considered an inverted bool test because
    	it returns true when not ratelimited.  Several tests in the
    	kernel tree use this __ratelimit() function incorrectly.
    
    	No net_ratelimit uses are incorrect currently though.
    
    	Most uses of net_ratelimit are to log something via printk or
    	pr_<level>.
    
    	In order to minimize the uses of net_ratelimit, and to start
    	standardizing the code style used for __ratelimit() and net_ratelimit(),
    	add a net_ratelimited_function() macro and net_<level>_ratelimited()
    	logging macros similar to pr_<level>_ratelimited that use the global
    	net_ratelimit instead of a static per call site "struct ratelimit_state".
    
    	Signed-off-by: Joe Perches <joe@perches.com>
    	Signed-off-by: David S. Miller <davem@davemloft.net>
    
    mcgrof@tux ~/compat (git::master)$ ckmake
    Trying kernel                  3.4.0-030400rc1-generic	[OK]
    Trying kernel                  3.3.0-030300rc2-generic	[OK]
    Trying kernel                     3.2.2-030202-generic	[OK]
    Trying kernel                    3.1.10-030110-generic	[OK]
    Trying kernel                    3.0.18-030018-generic	[OK]
    Trying kernel                  2.6.39-02063904-generic	[OK]
    Trying kernel                  2.6.38-02063808-generic	[OK]
    Trying kernel                  2.6.37-02063706-generic	[OK]
    Trying kernel                  2.6.36-02063604-generic	[OK]
    Trying kernel                  2.6.35-02063512-generic	[OK]
    Trying kernel                  2.6.34-02063410-generic	[OK]
    Trying kernel                  2.6.33-02063305-generic	[OK]
    Trying kernel                  2.6.32-02063255-generic	[OK]
    Trying kernel                  2.6.31-02063113-generic	[OK]
    Trying kernel                  2.6.30-02063010-generic	[OK]
    Trying kernel                  2.6.29-02062906-generic	[OK]
    Trying kernel                  2.6.28-02062810-generic	[OK]
    Trying kernel                    2.6.27-020627-generic	[OK]
    Trying kernel                    2.6.26-020626-generic	[OK]
    Trying kernel                    2.6.25-020625-generic	[OK]
    Trying kernel                    2.6.24-020624-generic	[OK]
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@frijolero.org>

--- include/linux/compat-3.5.h
+++ include/linux/compat-3.5.h
@@ -4,6 +4,7 @@
 #include <linux/version.h>
 #include <linux/fs.h>
 #include <linux/etherdevice.h>
+#include <linux/net.h>
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,5,0))
 
@@ -18,6 +19,29 @@ static inline bool ether_addr_equal(const u8 *addr1, const u8 *addr2)
 {
     return !compare_ether_addr(addr1, addr2);
 }
+
+#define net_ratelimited_function(function, ...)			\
+do {								\
+	if (net_ratelimit())					\
+		function(__VA_ARGS__);				\
+} while (0)
+
+#define net_emerg_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_emerg, fmt, ##__VA_ARGS__)
+#define net_alert_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_alert, fmt, ##__VA_ARGS__)
+#define net_crit_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_crit, fmt, ##__VA_ARGS__)
+#define net_err_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_err, fmt, ##__VA_ARGS__)
+#define net_notice_ratelimited(fmt, ...)			\
+	net_ratelimited_function(pr_notice, fmt, ##__VA_ARGS__)
+#define net_warn_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_warn, fmt, ##__VA_ARGS__)
+#define net_info_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_info, fmt, ##__VA_ARGS__)
+#define net_dbg_ratelimited(fmt, ...)				\
+	net_ratelimited_function(pr_debug, fmt, ##__VA_ARGS__)
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,5,0)) */
 
