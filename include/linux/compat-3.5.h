5a218cdb48fab47d6c0aea76ede99b415f02bc03
Ozan Çağlayan <ozancag@gmail.com>
2012-06-14 20:52:46 +0300

    compat: Backport fault_in_multipages_{writeable,readable}
    This backports:
    
    commit f56f821feb7b36223f309e0ec05986bb137ce418
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Sun Mar 25 19:47:41 2012 +0200
    
        mm: extend prefault helpers to fault in more than PAGE_SIZE
    
    The new functions are used by drm/i915 driver.
    
    Trying kernel                     3.4.0-030400-generic	[OK]
    Trying kernel                     3.3.7-030307-generic	[OK]
    Trying kernel                     3.2.2-030202-generic	[OK]
    Trying kernel                    3.1.10-030110-generic	[OK]
    Trying kernel                    3.0.18-030018-generic	[OK]
    Trying kernel                  2.6.39-02063904-generic	[OK]
    Trying kernel                  2.6.38-02063808-generic	[OK]
    Trying kernel                  2.6.37-02063706-generic	[OK]
    Trying kernel                  2.6.36-02063604-generic	[OK]
    Trying kernel                  2.6.35-02063512-generic	[OK]
    Trying kernel                  2.6.34-02063410-generic	[OK]
    Trying kernel                  2.6.33-02063305-generic	[OK]
    Trying kernel                  2.6.32-02063255-generic	[OK]
    Trying kernel                  2.6.31-02063113-generic	[OK]
    Trying kernel                  2.6.30-02063010-generic	[OK]
    Trying kernel                  2.6.29-02062906-generic	[OK]
    Trying kernel                  2.6.28-02062810-generic	[OK]
    Trying kernel                    2.6.27-020627-generic	[OK]
    Trying kernel                    2.6.26-020626-generic	[OK]
    Trying kernel                    2.6.25-020625-generic	[OK]
    Trying kernel                    2.6.24-020624-generic	[OK]
    
    Signed-off-by: Ozan Çağlayan <ozancag@gmail.com>
    Signed-off-by: Luis R. Rodriguez <mcgrof@frijolero.org>

--- include/linux/compat-3.5.h
+++ include/linux/compat-3.5.h
@@ -8,6 +8,73 @@
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,5,0))
 
+/*
+ * This backports:
+ *
+ * commit f56f821feb7b36223f309e0ec05986bb137ce418
+ * Author: Daniel Vetter <daniel.vetter@ffwll.ch>
+ * Date:   Sun Mar 25 19:47:41 2012 +0200
+ *
+ *     mm: extend prefault helpers to fault in more than PAGE_SIZE
+ *
+ * The new functions are used by drm/i915 driver.
+ *
+ */
+
+static inline int fault_in_multipages_writeable(char __user *uaddr, int size)
+{
+        int ret = 0;
+        char __user *end = uaddr + size - 1;
+
+        if (unlikely(size == 0))
+                return ret;
+
+        /*
+         * Writing zeroes into userspace here is OK, because we know that if
+         * the zero gets there, we'll be overwriting it.
+         */
+        while (uaddr <= end) {
+                ret = __put_user(0, uaddr);
+                if (ret != 0)
+                        return ret;
+                uaddr += PAGE_SIZE;
+        }
+
+        /* Check whether the range spilled into the next page. */
+        if (((unsigned long)uaddr & PAGE_MASK) ==
+                        ((unsigned long)end & PAGE_MASK))
+                ret = __put_user(0, end);
+
+        return ret;
+}
+
+static inline int fault_in_multipages_readable(const char __user *uaddr,
+                                               int size)
+{
+        volatile char c;
+        int ret = 0;
+        const char __user *end = uaddr + size - 1;
+
+        if (unlikely(size == 0))
+                return ret;
+
+        while (uaddr <= end) {
+                ret = __get_user(c, uaddr);
+                if (ret != 0)
+                        return ret;
+                uaddr += PAGE_SIZE;
+        }
+
+        /* Check whether the range spilled into the next page. */
+        if (((unsigned long)uaddr & PAGE_MASK) ==
+                        ((unsigned long)end & PAGE_MASK)) {
+                ret = __get_user(c, end);
+                (void)c;
+        }
+
+        return ret;
+}
+
 /* switcheroo is available on >= 2.6.34 */
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,34))
 #include <linux/vga_switcheroo.h>
