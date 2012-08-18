b8471d8a73c9dd1002b335f8f756257bc542ddeb
Hauke Mehrtens <hauke@hauke-m.de>
2012-08-18 13:46:54 +0200

    compat: add tty_port_register_device
    tty_register_device was replaced by tty_port_register_device in this
    commit:
    commit 734cc1783816ae358cef45673a29bf7af974e147
    Author: Jiri Slaby <jslaby@suse.cz>
    Date:   Tue Aug 7 21:47:47 2012 +0200
    
        TTY: use tty_port_register_device
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- include/linux/compat-3.7.h
+++ include/linux/compat-3.7.h
@@ -15,6 +15,9 @@ bool mod_delayed_work(struct workqueue_struct *wq, struct delayed_work *dwork,
 #define tty_lock(__tty) tty_lock()
 #define tty_unlock(__tty) tty_unlock()
 
+#define tty_port_register_device(port, driver, index, device) \
+	tty_register_device(driver, index, device)
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)) */
 
 #endif /* LINUX_3_7_COMPAT_H */
