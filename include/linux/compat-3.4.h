4292914d15181ad06aba9db77cc706c0374ada0a
Hauke Mehrtens <hauke@hauke-m.de>
2012-11-28 23:57:40 +0100

    compat: move config_enabled to compat-3.4.h
    config_enabled() was introduced in kernel version 3.4 and not in
    version 3.1. Now some driver uses this directly and this fails without
    this patch.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- include/linux/compat-3.4.h
+++ include/linux/compat-3.4.h
@@ -123,6 +123,20 @@ static inline void eth_hw_addr_random(struct net_device *dev)
 	module_driver(__pci_driver, pci_register_driver, \
 		       pci_unregister_driver)
 
+/*
+ * Getting something that works in C and CPP for an arg that may or may
+ * not be defined is tricky.  Here, if we have "#define CONFIG_BOOGER 1"
+ * we match on the placeholder define, insert the "0," for arg1 and generate
+ * the triplet (0, 1, 0).  Then the last step cherry picks the 2nd arg (a one).
+ * When CONFIG_BOOGER is not defined, we generate a (... 1, 0) pair, and when
+ * the last step cherry picks the 2nd arg, we get a zero.
+ */
+#define __ARG_PLACEHOLDER_1 0,
+#define config_enabled(cfg) _config_enabled(cfg)
+#define _config_enabled(value) __config_enabled(__ARG_PLACEHOLDER_##value)
+#define __config_enabled(arg1_or_junk) ___config_enabled(arg1_or_junk 1, 0)
+#define ___config_enabled(__ignored, val, ...) val
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,4,0)) */
 
 #endif /* LINUX_5_4_COMPAT_H */
