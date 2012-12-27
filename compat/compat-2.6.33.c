681f75854faf507d8cf128cf4cb03c9f991c40b0
Hauke Mehrtens <hauke@hauke-m.de>
2012-12-27 18:19:41 +0100

    compat: export platform_device_register_data()
    platform_device_register_data has to be exported for kernel
    version < 2.6.33. Some of these kernel have this method but do not
    export it and only kernel version < 2.6.28 does not have this function.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- compat/compat-2.6.33.c
+++ compat/compat-2.6.33.c
@@ -12,6 +12,7 @@
 #include <linux/device.h>
 #include <linux/usb.h>
 #include <linux/pm_runtime.h>
+#include <linux/platform_device.h>
 
 #ifdef CONFIG_USB_SUSPEND
 /**
@@ -172,3 +173,50 @@ EXPORT_SYMBOL_GPL(pcmcia_loop_tuple);
 
 #endif /* CONFIG_PCCARD */
 
+/**
+ * platform_device_register_data
+ * @parent: parent device for the device we're adding
+ * @name: base name of the device we're adding
+ * @id: instance id
+ * @data: platform specific data for this platform device
+ * @size: size of platform specific data
+ *
+ * This function creates a simple platform device that requires minimal
+ * resource and memory management. Canned release function freeing memory
+ * allocated for the device allows drivers using such devices to be
+ * unloaded without waiting for the last reference to the device to be
+ * dropped.
+ */
+struct platform_device *platform_device_register_data(
+		struct device *parent,
+		const char *name, int id,
+		const void *data, size_t size)
+{
+	struct platform_device *pdev;
+	int retval;
+
+	pdev = platform_device_alloc(name, id);
+	if (!pdev) {
+		retval = -ENOMEM;
+		goto error;
+	}
+
+	pdev->dev.parent = parent;
+
+	if (size) {
+		retval = platform_device_add_data(pdev, data, size);
+		if (retval)
+			goto error;
+	}
+
+	retval = platform_device_add(pdev);
+	if (retval)
+		goto error;
+
+	return pdev;
+
+error:
+	platform_device_put(pdev);
+	return ERR_PTR(retval);
+}
+EXPORT_SYMBOL_GPL(platform_device_register_data);
