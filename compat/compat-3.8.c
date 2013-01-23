670c4a3d46797a92fd15d201e5d9859393c7b2b6
Hauke Mehrtens <hauke@hauke-m.de>
2013-01-23 23:54:45 +0100

    compat: do not access default_ethtool_ops
    &default_ethtool_ops is not exported in kernel 3.7.4 and earlier and it
    should be be accessed from compat.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- compat/compat-3.8.c
+++ compat/compat-3.8.c
@@ -19,22 +19,12 @@
 #include <linux/netdevice.h>
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,5))
-
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0))
 void netdev_set_default_ethtool_ops(struct net_device *dev,
 				    const struct ethtool_ops *ops)
 {
 	if (!dev->ethtool_ops)
 		dev->ethtool_ops = ops;
 }
-#else /* kernel is between 3.7.0 and 3.7.4 */
-void netdev_set_default_ethtool_ops(struct net_device *dev,
-				    const struct ethtool_ops *ops)
-{
-	if (dev->ethtool_ops == &default_ethtool_ops)
-		dev->ethtool_ops = ops;
-}
-#endif
 
 EXPORT_SYMBOL_GPL(netdev_set_default_ethtool_ops);
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,5) */
