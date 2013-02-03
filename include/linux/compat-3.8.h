221c8c894f7bdf06f3aae61468c7dba7f3c16e55
Hauke Mehrtens <hauke@hauke-m.de>
2013-02-03 18:25:07 +0100

    compat: netdev_set_default_ethtool_ops() is not in kernel 3.7.5
    Upstream commit 726d9b2ca3f75cba9f1d03e519c2b2e8bf6523a7
    
    netdev_set_default_ethtool_ops() should be added to compat for every
    3.7.X series kernel as it is also not in 3.7.5.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/linux/compat-3.8.h
+++ include/linux/compat-3.8.h
@@ -9,10 +9,8 @@
 #include <linux/netdevice.h>
 #include <linux/efi.h>
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,5))
 extern void netdev_set_default_ethtool_ops(struct net_device *dev,
 					   const struct ethtool_ops *ops);
-#endif
 
 #define HID_BUS_ANY                            0xffff
 #define HID_GROUP_ANY                          0x0000
