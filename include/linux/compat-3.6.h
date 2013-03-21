cc35b309c5f605aa489098897d8bd57ae2f0abe7
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2013-03-21 23:29:23 +0000

    compat: backport sg_alloc_table_from_pages() from efc42bc9
    This backports sg_alloc_table_from_pages() added via commit efc42bc9
    
    mcgrof@frijol ~/linux-next (git::master)$ git describe --contains efc42bc9
    v3.6-rc1~57^2~11
    
    commit efc42bc98058a36d761b16a114823db1a902ed05
    Author: Tomasz Stanislawski <t.stanislaws@samsung.com>
    Date:   Mon Jun 18 09:25:01 2012 +0200
    
        scatterlist: add sg_alloc_table_from_pages function
    
        This patch adds a new constructor for an sg table. The table is constructed
        from an array of struct pages. All contiguous chunks of the pages are merged
        into a single sg nodes. A user may provide an offset and a size of a buffer if
        the buffer is not page-aligned.
    
        The function is dedicated for DMABUF exporters which often perform conversion
        from an page array to a scatterlist. Moreover the scatterlist should be
        squashed in order to save memory and to speed-up the process of DMA mapping
        using dma_map_sg.
    
        The code is based on the patch 'v4l: vb2-dma-contig: add support for
        scatterlist in userptr mode' and hints from Laurent Pinchart.
    
        Signed-off-by: Tomasz Stanislawski <t.stanislaws@samsung.com>
        Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
        Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
        Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
        Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
        CC: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- include/linux/compat-3.6.h
+++ include/linux/compat-3.6.h
@@ -5,6 +5,15 @@
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,6,0))
 
+#include <linux/scatterlist.h>
+
+/* backports efc42bc9 */
+#define sg_alloc_table_from_pages LINUX_BACKPORT(sg_alloc_table_from_pages)
+int sg_alloc_table_from_pages(struct sg_table *sgt,
+			      struct page **pages, unsigned int n_pages,
+			      unsigned long offset, unsigned long size,
+			      gfp_t gfp_mask);
+
 /**
  * Backports
  *
